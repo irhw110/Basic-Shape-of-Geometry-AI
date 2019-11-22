59cca44cb322041b3703703e3dae56db3f3bfc3d
Naufal Zhafran <latifnaufal7@gmail.com>
2019-11-23 05:13:39 +0700

    Change to approxpolydp

--- src/KeyFeatureDetector.py
+++ src/KeyFeatureDetector.py
@@ -22,25 +22,20 @@ class KeyFeatureDetector():
             size = cv.contourArea(contour)
             rect = cv.minAreaRect(contour)
 
-            gray = np.float32(self._image_gray)
-            mask = np.zeros(gray.shape, dtype="uint8")
-            cv.fillPoly(mask, [contour], (255,255,255))
-            dst = cv.cornerHarris(mask,5,3,0.04)
-            ret, dst = cv.threshold(dst,0.1*dst.max(),255,0)
-            dst = np.uint8(dst)
-            ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
-            criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-            corners = cv.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
-
-            for i in range(1, len(corners)):
-                print(corners[i])
-
-            self._image[dst>0.1*dst.max()]=[0,0,255]
+            peri = cv.arcLength(contour, True)
+            approx = cv.approxPolyDP(contour, 0.001 * peri, True)
+            self._image = cv.drawContours(self._image, [approx], 0, (0,255,0), 3)
+            
+            print(approx)
+            
+    
+    def showImage(self):
             cv.imshow('image', self._image)
             cv.waitKey(0)
             cv.destroyAllWindows
 
 if __name__ == "__main__":
-    Detector = KeyFeatureDetector('../shapes/segilima_beraturan.png')
+    Detector = KeyFeatureDetector('shapes/segitiga_lancip.png')
     Detector.readFile()
     Detector.detectCorner()
+    Detector.showImage()
